4e512d4b3718bfe8a1b570441a3655259633fe64
zhl <1786524543@qq.com>
2022-04-07 08:01:40 +0800

    first commit

---
+++ src/views/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>vue3.0 初体验</h1>
+    <p>少年你的头发可还好，？？？？ 哈哈哈哈哈</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.test {
+  color: red;
+}
+</style>
